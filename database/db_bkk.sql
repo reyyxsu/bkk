afdd9f2386c003847a65c87c906a86bb14e83978
reyyxsu <rylzrhn06@gmail.com>
2024-10-07 13:27:25 +0700

    penambahan field foto_pas, foto_ijazah, foto_sk_kerja di table data_siswa

--- database/db_bkk.sql
+++ database/db_bkk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2024 at 06:06 AM
+-- Generation Time: Oct 07, 2024 at 08:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -58,18 +58,22 @@ CREATE TABLE `data_siswa` (
   `alamat` varchar(100) DEFAULT NULL,
   `tb` varchar(5) DEFAULT NULL,
   `bb` varchar(50) DEFAULT NULL,
-  `pendidikan_terakhir` varchar(50) DEFAULT NULL
+  `pendidikan_terakhir` varchar(50) DEFAULT NULL,
+  `foto_pas` varchar(500) NOT NULL,
+  `foto_ijazah` varchar(500) NOT NULL,
+  `foto_sk_kerja` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `data_siswa`
 --
 
-INSERT INTO `data_siswa` (`id`, `nama`, `alamat`, `tb`, `bb`, `pendidikan_terakhir`) VALUES
-(6, 'Faleniko Mubintoro', 'Perumahan Wisma Asri', '180', '75', 'SMK Negeri 1 Jomokerto'),
-(7, 'Ariyal Zarhan Ikhlas Haryanto', 'Perumahan Pesona Anggrek', '180', '75', 'SMK Negeri 5 Kota Bekasi'),
-(8, 'Muhammad Atallah', 'Perumahan Wisma Asri', '175', '76', 'SMK Negeri 5 Kota Bekasi'),
-(9, 'Bryllent Arcielio Lim', 'Perumahan Wisma Asri', '176', '65', 'SMK Negeri 1 Jomokerto');
+INSERT INTO `data_siswa` (`id`, `nama`, `alamat`, `tb`, `bb`, `pendidikan_terakhir`, `foto_pas`, `foto_ijazah`, `foto_sk_kerja`) VALUES
+(6, 'Faleniko Mubintoro', 'Perumahan Wisma Asri', '180', '75', 'SMK Negeri 1 Jomokerto', '', '', ''),
+(7, 'Ariyal Zarhan Ikhlas Haryanto', 'Perumahan Pesona Anggrek', '180', '75', 'SMK Negeri 5 Kota Bekasi', '', '', ''),
+(8, 'Muhammad Atallah', 'Perumahan Wisma Asri', '175', '76', 'SMK Negeri 5 Kota Bekasi', '', '', ''),
+(9, 'Bryllent Arcielio Lim', 'Perumahan Wisma Asri', '176', '65', 'SMK Negeri 1 Jomokerto', '', '', ''),
+(10, 'Sprei Gratis', 'Perumahan Wisma Asri', '123', '456', 'SMK Negeri 5 Kota Bekasi', '1728281126_2024-09-07_22_36_18.png', '1728281126_2024-09-15_13_19_10.png', '1728281126_2024-09-07_23_48_12.png');
 
 -- --------------------------------------------------------
 
@@ -126,7 +130,7 @@ ALTER TABLE `data_perusahaan`
 -- AUTO_INCREMENT for table `data_siswa`
 --
 ALTER TABLE `data_siswa`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
